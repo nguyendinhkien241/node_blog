9e8599973195de9223f51e786985f1c3294429c4
nguyendinhkien241 <nguyendinhkien241@gmail.com>
2024-08-12 14:59:59 +0700

    fix

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ const morgan = require('morgan');
 const { engine } = require('express-handlebars');
 const app = express();
 const port = 3000;
-
+const db = require('./config/db');
 const route = require('./routes');
 
 //HTTP logger
 app.use(morgan('combined'));
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+db.connect();
 
 //route init
 route(app);
@@ -30,8 +32,8 @@ app.engine(
     }),
 );
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, '/resources/views'));
+app.set('views', path.join(__dirname, 'resources','views'));
 
 app.listen(port, () =>
-    console.log(`Example app listening at http://localhost:${port}`),
+    console.log(`App listening at http://localhost:${port}`),
 );
