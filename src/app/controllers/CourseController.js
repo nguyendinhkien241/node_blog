f4cd34758dd5bf23b09522a8ee9499fe2977db8b
nguyendinhkien241 <nguyendinhkien241@gmail.com>
2024-08-13 23:25:22 +0700

    read from mongoose

---
+++ src/app/controllers/CourseController.js
@@ -0,0 +1,19 @@
+const Course = require('../models/Course');
+const { multipleMongooseToObject } = require('../../util/mongoose');
+const { mongooseToObject } = require('../../util/mongoose');
+
+class CourseController {
+
+    show(req, res, next) {
+        Course.findOne({
+            slug: req.params.slug
+        })
+            .then((course) => {
+                res.render('courses/show', {course: mongooseToObject(course) })
+            })
+            .catch(next);
+    }
+
+}
+
+module.exports = new CourseController();
--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,12 +1,16 @@
 const Course = require('../models/Course');
+const { multipleMongooseToObject } = require('../../util/mongoose');
 
 class SiteController {
     index(req, res) {
         Course.find({})
-            .then((courses) => res.json(courses))
+            .then((courses) => {
+                
+                res.render('home', { 
+                    courses: multipleMongooseToObject(courses)
+                });
+            })
             .catch((err)=> res.status(400).json({err: 'ERROR!!'}))
-
-        // res.render('home');
     }
 
     search(req, res) {
